b663c7b0eee88d37ab015b8707b120d8c553f284
Victor <vicstor@bk.ru>
2019-10-25 17:02:11 +0300

    add first gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
+  get 'persons/profile', as: 'user_root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'home#index'
 end
